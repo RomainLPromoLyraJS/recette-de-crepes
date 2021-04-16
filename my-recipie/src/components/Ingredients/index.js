b2c80e871c99963337e9da0a0760fa7f6bf14f08
RomainLPromoLyraJS <romain75l@gmail.com>
2021-04-16 15:54:09 +0200

    Ingredients component ended in static

--- my-recipie/src/components/Ingredients/index.js
+++ my-recipie/src/components/Ingredients/index.js
@@ -6,8 +6,28 @@ import '../Ingredients';
 
 const Ingredients = () => {
     return (
-        <div>
-            
+        <div className="ingredients">
+            <div className= "ingredients__instructions">
+                <p><span className="ingredients__instructions__span">375 g</span> de farine tamisée</p>
+            </div>
+            <div className= "ingredients__instructions">
+                <p><span className="ingredients__instructions__span">3 pincées</span> de sel</p>
+            </div>
+            <div className= "ingredients__instructions">
+                <p><span className="ingredients__instructions__span">50 g</span> de sucre en poudre</p>
+            </div>
+            <div className= "ingredients__instructions">
+                <p><span className="ingredients__instructions__span">6</span> oeufs entiers</p>
+            </div>
+            <div className= "ingredients__instructions">
+                <p><span className="ingredients__instructions__span">80 g</span> de beurre fondu</p>
+            </div>
+            <div className= "ingredients__instructions">
+                <p><span className="ingredients__instructions__span">1 L</span> de lait</p>
+            </div>
+            <div className= "ingredients__instructions">
+                <p><span className="ingredients__instructions__span">3 cuillerées à soupe</span> de rhum ambré ou vieux</p>
+            </div>
         </div>
     );
 };
